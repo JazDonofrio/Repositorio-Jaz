9ec7694e8464de1742e8978fee7da393be511149
JazDonofrio <jazmindonofrio@gmail.com>
2021-10-05 14:30:25 -0300

    resolucion ejercicio 1 intermedio

---
+++ Ejercicios intermedios 1.py
@@ -0,0 +1,8 @@
+# Ejercicio 1 
+#   fuente: https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-de-listas-tuplas-y-diccionarios-en-python/
+#   Crea una tupla con números, pide un numero por teclado e indica cuantas veces se repite.
+numero = int(input("Ingrese un numero: "))
+
+tupla = 1, 2, 3 , 3, 3
+
+print(tupla.count(numero)) #count cuenta la cantidad veces que aparece un elemento
